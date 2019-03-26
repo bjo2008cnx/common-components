97d54fd37d62034f80520114b28a6bf378576bdf
michael <michael@weimob.com>
2019-03-26 15:36:54 +0800

    init

--- examples/src/test/java/com/github/common/componets/RenameFile.java
+++ examples/src/test/java/com/github/common/componets/RenameFile.java
@@ -1,4 +1,4 @@
-package com.github.common.components.rpc.serializer;
+package com.github.common.componets;
 
 import java.io.File;
 import java.io.IOException;
@@ -9,7 +9,9 @@ import java.io.IOException;
  * @Description: 批量重命名文件
  */
 public class RenameFile {
+    public static final String OLD_DIR = "abc";
     static String dir = "/Users/michael/codes/codes-mine/common-components";//文件所在路径，所有文件的根目录，记得修改为你电脑上的文件所在路径
+
 
     public static void main(String[] args) throws IOException {
         recursiveTraversalFolder(dir);//递归遍历此路径下所有文件夹
@@ -18,14 +20,15 @@ public class RenameFile {
     /**
      * 递归遍历文件夹获取文件
      */
-    public static void recursiveTraversalFolder(String path) {
+    public static void recursiveTraversalFolder(String path) throws IOException {
         File folder = new File(path);
         File[] fileArr = folder.listFiles();
         for (File file : fileArr) {
             if (file.isDirectory()) {
                 //是文件夹，继续递归，如果需要重命名文件夹，这里可以做处理
-                if (file.getName().contains("quzeng")) {
-                    file.renameTo(new File("github"));//重命名
+                if (file.getName().contains(OLD_DIR)) {
+                    System.out.println(file.getCanonicalPath());
+                    file.renameTo(new File(file.getCanonicalPath().replaceAll(OLD_DIR, "github")));//重命名
                 } else {
                     recursiveTraversalFolder(file.getAbsolutePath());
                 }
